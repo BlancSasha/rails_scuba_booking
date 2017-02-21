bc5a85aac9fb696a93291da7f3f2e32d94c20982
martin peubey <martin.peubey@gmail.com>
2017-02-21 12:09:57 +0100

    generate controller Dives Users Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,42 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'bookings/index'
+
+  get 'bookings/show'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/edit'
+
+  get 'bookings/update'
+
+  get 'bookings/destroy'
+
+  get 'dives/index'
+
+  get 'dives/show'
+
+  get 'dives/new'
+
+  get 'dives/create'
+
+  get 'dives/edit'
+
+  get 'dives/update'
+
+  get 'dives/destroy'
+
   devise_for :users
   root to: 'pages#home'
 

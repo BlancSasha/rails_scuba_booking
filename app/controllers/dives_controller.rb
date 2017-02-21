c19ce3ecd4f0957b172e951bcf7dbfda0d0f2f3a
martin peubey <martin.peubey@gmail.com>
2017-02-21 15:18:55 +0100

    implement controller dives

--- app/controllers/dives_controller.rb
+++ app/controllers/dives_controller.rb
@@ -1,22 +1,38 @@
 class DivesController < ApplicationController
   def index
+    @dives = Dive.all
   end
 
   def show
+    @dive = Dive.find(params[:id])
   end
 
   def new
+    @dive = Dive.new
   end
 
   def create
+    @dive = Dive.new(dive_params)
+    if @dive.save
+      redirect_to dive_path(@dive)
+    end
   end
 
   def edit
+    @dive = Dive.find(params[:id])
   end
 
   def update
+    @dive = Dive.find(params[:id])
+    @dive.update(dive_params)
   end
 
   def destroy
+    @dive = Dive.find(params[:id])
+    @dive.destroy
+  end
+
+  def dive_params
+    params.require(:dive).permit(:location, :capacity, :price, :depth_required, :description)
   end
 end

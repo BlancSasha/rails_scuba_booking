359e1b4c8fc12c42576e8460cdb91ce6eabb6d22
Romain TISSEAU <rt.network@gmx.com>
2017-02-21 15:06:22 +0100

    update User Model - Migration pour ajouer Teacher, Bio, Age - Migration valuer par défaut de Teacher = false

--- app/models/dive.rb
+++ app/models/dive.rb
@@ -3,9 +3,9 @@ class Dive < ApplicationRecord
   has_many :bookings
   validates :location, :capacity, :price, :depth_required, :description, presence: true
   validates :depth_required, inclusion: { in: (0..40) }
-  validate :user_can_be_a_teacher
+  validate :user_is_a_teacher
 
-  def user_can_be_a_teacher
-    errors.add(:user, "can't be a teacher") unless user.level >= 4
+  def user_is_a_teacher
+    errors.add(:user, "is not a teacher") unless user.teacher == true
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,17 @@ class User < ApplicationRecord
   has_many :dives
   validates :name, presence: true
   validates :level, inclusion: { in: (1..5) }
+  validate :user_can_be_a_teacher
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  protected
+
+  def user_can_be_a_teacher
+    if level < 4
+      errors.add(:teacher, "can't be a teacher") unless teacher == false
+    else
+      true
+    end
+  end
 end

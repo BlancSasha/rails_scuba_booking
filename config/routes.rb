30819647e7c7d19f3cd1ca22847c0d7227c84a28
Francois Blanc <francois.blanc73@gmail.com>
2017-02-21 10:54:02 +0100

    Added all routes and corrected dives singular in inflections.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users
+
+  resources :dives do
+    resources :bookings
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

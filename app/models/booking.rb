42913d61441ff260112a8abbb2e29ef2d0817eee
Romain TISSEAU <rt.network@gmx.com>
2017-02-21 11:27:12 +0100

    Crct Model's bug - DRY Models - Add custom validation for Booking's Model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dive
-  validates :user, :dive, presence: true
+  validate :quantity_cannot_be_greater_than_capacity
   # valider que quantity <= self.dive.capacity ? utiliser callback ?
+
+  def quantity_cannot_be_greater_than_capacity
+    errors.add(:quantity, "can't be greater than the capacity") unless quantity <= self.dive.capacity
+  end
 end
--- app/models/dive.rb
+++ app/models/dive.rb
@@ -1,7 +1,6 @@
 class Dive < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
   has_many :bookings
   validates :location, :capacity, :price, :depth_required, :description, presence: true
   validates :depth_required, inclusion: { in: (0..40) }
-
 end

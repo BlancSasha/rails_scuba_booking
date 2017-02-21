fcc36457e89c567ca782b0e4b7e4c936e67e93df
Francois Blanc <francois.blanc73@gmail.com>
2017-02-21 16:54:04 +0100

    Implemented users_controller and new and edit views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  def new
+  before_action :set_user, only: [:edit_teacher_status]
+
+  # new, create, edit, update et destroy sont normalement gérés
+  # par Devise dans registrations_controller
+
+  def edit_teacher_status
+    @user.teacher = !@user.teacher
   end
 
-  def create
-  end
+  private
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def level_collection
+    [["Level 1 : Open Water", 1],
+     ["Level 2 : Advanced", 2],
+     ["Level 3 : Rescue diver", 3],
+     ["Level 4 : Dive guide (can be teacher)", 4],
+     ["Level 5 : Dive instructor (can be teacher)", 5]]
+  end
 end

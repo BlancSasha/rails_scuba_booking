382f21314752f3bf984d797c72366ba99ceb3e42
Antoine Coulaud <antoinecoulaud@outlook.com>
2017-02-22 12:28:57 +0100

    add geocoding by location

--- app/models/dive.rb
+++ app/models/dive.rb
@@ -6,9 +6,13 @@ class Dive < ApplicationRecord
   validates :depth_required, inclusion: { in: (0..40) }
   validate :user_is_a_teacher
   has_attachments :photos, maximum: 2
+  geocoded_by :location
+  after_validation :geocode, if: :location_changed?
 
   def user_is_a_teacher
     errors.add(:user, "is not a teacher") unless user.teacher == true
   end
 
 end
+
+

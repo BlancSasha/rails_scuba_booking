2e7f6d9ca76ffaa6cfe8d181ab498b09d04a5823
Romain TISSEAU <rt.network@gmx.com>
2017-02-23 16:47:03 +0100

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   patch '/users/:id', to: 'users#update_teacher_status', as: 'teacher'
 
-  resources :users, except: [:index] do
+  resources :users, only: [:show] do
     resources :bookings, except: [:new, :show]
   end
 

fc4be8e92bb105ca2f650dcf32a1309626ed9ae9
Romain TISSEAU <rt.network@gmx.com>
2017-02-23 15:55:00 +0100

    Add method for guide/dives

--- app/controllers/guide/dives_controller.rb
+++ app/controllers/guide/dives_controller.rb
@@ -5,10 +5,50 @@ class Guide::DivesController < ApplicationController
   end
 
   def new
-
+    @dive = Dive.new
   end
 
   def create
+    @dive = Dive.new(dive_params)
+    if @dive.save
+      redirect_to dive_path(@dive)
+    else
+      render :new
+    end
+  end
 
+  def edit
+    @dive = Dive.find(params[:id])
+  end
+
+  def update
+    @dive = Dive.find(params[:id])
+    if @dive.update(dive_params)
+      redirect_to dive_path(@dive)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dive = Dive.find(params[:id])
+    # redirect_to profile_dives_path
+    ## Rediriger vers la index de Dives namespacé dans #profile
+  end
+
+
+  protected
+
+  def dive_params
+    params
+      .require(:dive)
+      .permit(
+        :location,
+        :capacity,
+        :price,
+        :depth_required,
+        :description,
+        photos: []
+      )
   end
 end

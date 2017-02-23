2bd2c1ebd9d1c7f2a78a646921bd66609a4878e0
Antoine Coulaud <antoinecoulaud@outlook.com>
2017-02-23 13:03:41 +0100

    delete namaspace

--- app/controllers/guide/dives_controller.rb
+++
@@ -1,6 +0,0 @@
-class Guide::DivesController < ApplicationController
-  def index
-    # Let's anticipate on next week (with login)
-    @dives = current_user.dives
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,8 @@ Rails.application.routes.draw do
 
   resources :dives
 
-  namespace :guide do
-    resources :dives, only: [:index]
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
 end
+
+

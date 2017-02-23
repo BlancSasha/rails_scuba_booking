8a88966be134f0739e04999e955d0d30d9be7f51
Antoine Coulaud <antoinecoulaud@outlook.com>
2017-02-23 15:08:32 +0100

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
 
   namespace :guide do
     resources :dives, only: [:index]
-    resources :bookings, only: [:index]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

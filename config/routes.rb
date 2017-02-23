f08aacad6c364ff9e66094257daea08a02dc3407
Romain TISSEAU <rt.network@gmx.com>
2017-02-23 16:12:54 +0100

    custom all routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
 
   patch '/users/:id', to: 'users#update_teacher_status', as: 'teacher'
 
-  resources :users, only: [:show] do
-    resources :bookings
+  resources :users, except: [:index] do
+    resources :bookings, except: [:new]
   end
 
-  resources :dives
+  resources :dives, only: [:index, :show]
 
   namespace :guide do
-    resources :dives, only: [:index, :new, :create]
+    resources :dives, except: [:show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

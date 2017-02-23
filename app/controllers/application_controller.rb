27d665a66fe2d6325659f5835461b43d7494c75b
Romain TISSEAU <rt.network@gmx.com>
2017-02-23 12:30:22 +0100

    overcoat Devise Controller for permission with leveln max_depth and name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:level, :max_depth, :name, :email, :password, :password_confirmation)
+    end
+  end
 end
